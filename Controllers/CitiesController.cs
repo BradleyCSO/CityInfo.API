211bc6140d4a1645f6c9225b72832e1ebe910927
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-09-22 11:19:24 +0100

    Move routing to class template from GetCities

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -3,9 +3,10 @@
 namespace CityInfo.API.Controllers
 {
     [ApiController] // Not necessary but improves development experience when building APIs, this should be documented as we go on
+    [Route("api/cities")]
     public class CitiesController : ControllerBase // Using controller base here as a derived class. If we inherited from the Controller class, it has additional helper methods for when returning views1q
     {
-        [HttpGet("api/cities")]
+        [HttpGet]
         public JsonResult GetCities()
         {
             return new JsonResult(new List<object>
