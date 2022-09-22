c30da1e2b066d35de4afa37967b599699c950a82
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-09-22 14:56:13 +0100

    Add GetFile to API (gets PDF)

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            // Can use FileContentResult, FileStreamResult, PhysicalFileResult, VirtualFileResult : all which derive from the same class
+            // For now, we will return File() which is defined as part of the controller, and acts as a wrapper around the forementioned classes
+            string filePath = "blank.pdf";
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+            
+            var bytes = System.IO.File.ReadAllBytes(filePath);
+
+            return File(bytes, "application/pdf", Path.GetFileName(filePath));
+        }
+    }
+}
