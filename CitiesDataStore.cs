ee216f131a771f0647b26deca18b673d49095f34
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-09-22 14:19:46 +0100

    Add API child resource

--- CitiesDataStore.cs
+++ CitiesDataStore.cs
@@ -15,13 +15,43 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "NYC",
-                    Description = "The Big Apple..."
+                    Description = "The Big Apple...",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "A very popular tourist destination"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "London",
-                    Description = "Big ben and big red busses."
+                    Description = "Big ben and big red busses.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 5,
+                            Name = "Big Ben",
+                            Description = "A very popular tourist destination"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 6,
+                            Name = "Tower Bridge",
+                            Description = "Not to be confused with London Bridge..."
+                        }
+                    }
                 },
             };
         }
---
+++ Controllers/PointsOfInterestController.cs
@@ -0,0 +1,44 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(get => get.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound(); // In this case we should return a 404 because if the city passed within cityId doesn't exist, because the URI itself wouldn't be pointing to a mapped resource
+            }
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{pointofinterestid}")]
+        public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(get => get.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound(); 
+            }
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(get => get.Id == pointOfInterestId);
+
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
+    }
+}
