3d68f93043e947bb201bb2f756f8ee3f3190d61d
Bradley Osborne <Bradley.Osborne@3csharedservices.org>
2022-10-25 12:59:51 +0100

    Add new points of interest post method

--- Controllers/PointsOfInterestController.cs
+++ Controllers/PointsOfInterestController.cs
@@ -21,7 +21,7 @@ namespace CityInfo.API.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{pointofinterestid}")]
+        [HttpGet("{pointofinterestid}", Name = "GetPointOfInterest")]
         public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(get => get.Id == cityId);
@@ -39,6 +39,41 @@ namespace CityInfo.API.Controllers
             }
 
             return Ok(pointOfInterest);
+        }
+
+        [HttpPost]
+        public ActionResult<PointOfInterestDto> CreatePointOfInterest(
+            int cityId, 
+            PointOfInterestForCreationDto pointOfInterest)
+        {
+            // For when a user sends a request for a resource URI that does not exist/trying to add a
+            // Point of interst for a city that does not exist
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            // Add point of interest by calculating the ID of the new point of interest
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId, // Take maxPointOfInterestID and add 1
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description,
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest", new
+            {
+                cityId = cityId,
+                pointOfInterestId = finalPointOfInterest.Id
+            },
+            finalPointOfInterest // Add to response body
+            );
         }
     }
 }
---
+++ Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CityInfo.API.Models
+{
+    // A model for which the consumer is responsible for updating, thus the ID has been omitted
+    // General principle: use a separate DTO for creating, updating and returning resources
+    public class PointOfInterestForCreationDto 
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
