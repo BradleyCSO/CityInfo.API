364c0ed1eb40ec551978d14d903c96027bc7d084
Bradley Osborne <Bradley.Osborne@3csharedservices.org>
2022-10-25 13:39:26 +0100

    Add DataAnnotation validation to property felds

--- Models/PointOfInterestForCreationDto.cs
+++ Models/PointOfInterestForCreationDto.cs
@@ -1,10 +1,15 @@
-﻿namespace CityInfo.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
 {
     // A model for which the consumer is responsible for updating, thus the ID has been omitted
     // General principle: use a separate DTO for creating, updating and returning resources
     public class PointOfInterestForCreationDto 
     {
+        [Required(ErrorMessage = "You should provide a name value.")]
+        [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
+        [MaxLength(200)]
         public string? Description { get; set; }
     }
 }
