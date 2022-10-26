62abb07e1f63d4941c1a04de9f6f9e72bdff85f2
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-10-26 13:36:49 +0100

    Create db context

---
+++ DbContexts/CityInfoContext.cs
@@ -0,0 +1,23 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DbContexts
+{
+    public class CityInfoContext : DbContext // Property values initialised as non-null in DbContext after leaving the base ctor
+    {
+        public DbSet<City> Cities { get; set; } = null!; // Null-forgiving operator
+        public DbSet<PointOfInterest> PointOfInterests { get; set; } = null!;
+
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) :
+            base(options)
+        {
+
+        }
+        // One way to configure db context
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("connectionstring");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using CityInfo.API;
+using CityInfo.API.DbContexts;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -37,6 +39,8 @@ builder.Services.AddTransient<IMailService, LocalMailService>(); // Whenever we 
 builder.Services.AddTransient<IMailService, CloudMailService>();
 #endif
 builder.Services.AddSingleton<CitiesDataStore>();
+
+builder.Services.AddDbContext<CityInfoContext>(dbContextOptions => dbContextOptions.UseSqlite("Data Source=CityInfo.db"));
 
 var app = builder.Build();
 
