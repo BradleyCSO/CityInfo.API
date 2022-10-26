b650935a193f38c60d2ebc5b2b6ca6021bd8ef2c
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-10-26 13:18:02 +0100

    Create Entity classes

---
+++ Entities/City.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // A new key will be generated when a City is added
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        public string? Description { get; set; }
+        public ICollection<PointOfInterest> PointsOfInterest { get; set; } = new List<PointOfInterest>();
+
+        public City(string name) // We want this City class to always have a name
+        {
+            Name = name;
+        }
+    }
+}
