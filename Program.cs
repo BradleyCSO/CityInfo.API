b848d3aee64436f330a1ea6a0848c7288e75bb65
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-10-26 14:58:58 +0100

    Update to get connection string from appsettings.json

--- Program.cs
+++ Program.cs
@@ -40,7 +40,9 @@ builder.Services.AddTransient<IMailService, CloudMailService>();
 #endif
 builder.Services.AddSingleton<CitiesDataStore>();
 
-builder.Services.AddDbContext<CityInfoContext>(dbContextOptions => dbContextOptions.UseSqlite("Data Source=CityInfo.db"));
+builder.Services.AddDbContext<CityInfoContext>(
+    dbContextOptions => dbContextOptions.UseSqlite(
+    builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
 
 var app = builder.Build();
 
