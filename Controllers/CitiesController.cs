9465973e929c7ef11ae954a3e5955d60ae186099
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-10-26 16:15:15 +0100

    Return data asynchronously via added repository

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -1,4 +1,5 @@
 ﻿using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -7,29 +8,45 @@ namespace CityInfo.API.Controllers
     [Route("api/cities")]
     public class CitiesController : ControllerBase // Using controller base here as a derived class. If we inherited from the Controller class, it has additional helper methods for when returning views1q
     {
-        private readonly CitiesDataStore _citiesDataStore;
-        public CitiesController(CitiesDataStore citiesDataStore)
+        private readonly ICityInfoRepository _cityInfoRepository;
+
+        public CitiesController(ICityInfoRepository cityInfoRepository)
         {
-            _citiesDataStore = citiesDataStore ?? throw new ArgumentNullException(nameof(citiesDataStore));
+            _cityInfoRepository = cityInfoRepository ?? throw new ArgumentNullException(nameof(cityInfoRepository));
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<CityDto>> GetCities()
+        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities()
         {
-            return Ok(_citiesDataStore.Cities); // No not found as an empty collection would be a valid response body
-        }
+            var cityEntities = await _cityInfoRepository.GetCitiesAsync();
+            
+            var results = new List<CityWithoutPointsOfInterestDto>();
 
-        [HttpGet("{id}")]
-        public ActionResult<CityDto> GetCity(int id)
-        {
-            var cityToReturn = _citiesDataStore.Cities.FirstOrDefault(get => get.Id == id);
-
-            if (cityToReturn == null)
+            foreach (var cityEntity in cityEntities)
             {
-                return NotFound();
+                results.Add(new CityWithoutPointsOfInterestDto
+                {
+                    Id = cityEntity.Id,
+                    Name = cityEntity.Name,
+                    Description = cityEntity.Description,
+                });
             }
 
-            return Ok(cityToReturn);
+            return Ok(results);
+            //return Ok(_citiesDataStore.Cities); // No not found as an empty collection would be a valid response body
         }
+
+        //[HttpGet("{id}")]
+        //public ActionResult<CityDto> GetCity(int id)
+        //{
+        //    //var cityToReturn = _citiesDataStore.Cities.FirstOrDefault(get => get.Id == id);
+
+        //    //if (cityToReturn == null)
+        //    //{
+        //    //    return NotFound();
+        //    //}
+
+        //    //return Ok(cityToReturn);
+        //}
     }
 }
