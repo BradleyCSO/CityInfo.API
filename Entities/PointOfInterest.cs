1b5aeda66eccd5547c0fc528fc2c04cad3ec3a7f
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-10-26 14:04:24 +0100

    Add migration via EntityFrameworkCore.Tools

--- Entities/PointOfInterest.cs
+++ Entities/PointOfInterest.cs
@@ -12,6 +12,8 @@ namespace CityInfo.API.Entities
         [MaxLength(50)]
         public string Name { get; set; }
         [ForeignKey("CityId")]
+        [MaxLength(200)]
+        public string Description { get; set; }
         public City? City { get; set; } // Navigation property. Foreign key -- thus a relationship between these classes will be created
         public int CityId { get; set; }
         
