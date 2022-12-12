767d8dd01333ec5e19532436d0e36e5dcf2a8513
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-12-12 15:52:15 +0000

    Implement pagination at database level

--- Services/CityInfoRepository.cs
+++ Services/CityInfoRepository.cs
@@ -18,14 +18,9 @@ namespace CityInfo.API.Services
             return await _context.Cities.OrderBy(c => c.Name).ToListAsync();
         }
 
-        public async Task<IEnumerable<City>> GetCitiesAsync(string? name, string? searchQuery)
+        public async Task<IEnumerable<City>> GetCitiesAsync(
+            string? name, string? searchQuery, int pageNumber, int pageSize)
         {
-            if (string.IsNullOrEmpty(name) && 
-                string.IsNullOrWhiteSpace(searchQuery))
-            {
-                return await GetCitiesAsync();
-            }
-
             // Collection to start from, for deferred execution
             var collection = _context.Cities as IQueryable<City>; 
 
@@ -42,7 +37,11 @@ namespace CityInfo.API.Services
                 || (a.Description != null && a.Description.Contains(searchQuery)));
             }
 
-            return await collection.OrderBy(c => c.Name).ToListAsync();
+            return await collection
+                .OrderBy(c => c.Name)
+                .Skip(pageSize * (pageNumber - 1))
+                .Take(pageSize)
+                .ToListAsync();
         }
 
         public async Task<bool> CityExistsAsync(int cityId)
