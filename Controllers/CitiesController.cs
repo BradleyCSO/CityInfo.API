82fb752f535364f4668a663d9b25569c9e744488
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-12-12 15:22:37 +0000

    Add searchQuery filter to get cities method

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -21,9 +21,9 @@ namespace CityInfo.API.Controllers
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities(
-            [FromQuery] string? name)
+            [FromQuery] string? name, string? searchQuery)
         {
-            var cityEntities = await _cityInfoRepository.GetCitiesAsync(name);
+            var cityEntities = await _cityInfoRepository.GetCitiesAsync(name, searchQuery);
             return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities));
 
             //var results = new List<CityWithoutPointsOfInterestDto>();
