0084dbdc5c8d448a71de2f33c391250ff9270be5
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-12-13 14:24:11 +0000

    Document possible responses for GetCity action

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -63,7 +63,11 @@ namespace CityInfo.API.Controllers
         /// <param name="id">The id of the city to get</param>
         /// <param name="includePointsOfInterest">Whether or not to include the points of interest</param>
         /// <returns>An IActionResult</returns>
+        /// <response code="200">Returns the requested city</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetCity(int id, bool includePointsOfInterest = false)
         {
             var city = await _cityInfoRepository.GetCityAsync(id, includePointsOfInterest);
