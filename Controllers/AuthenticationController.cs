cca1fdeb3db00aa0b758b44ed0a847acd8f25fdd
BradleyCSO <Osborne.Bradley1998@gmail.com>
2022-12-13 10:34:24 +0000

    Creating a user-password token for API

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        // Won't be used outside of this class, so can scope it to this namespace
+        public class AuthenticationRequestBody
+        {
+            public string? UserName { get; set; }
+            public string? Password { get; set; }
+        }
+        private class CityInfoUser
+        {
+            public int UserId { get; set; }
+            public string UserName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string City { get; set; }
+
+            public CityInfoUser(
+                int userId,
+                string userName,
+                string firstName,
+                string lastName,
+                string city)
+            {
+                UserId = userId;
+                UserName = userName;
+                FirstName = firstName;
+                LastName = lastName;
+                City = city;
+            }
+        }
+
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        [HttpPost("authenticate")]
+        public ActionResult<string> Authenticate(
+            AuthenticationRequestBody authenticationRequestBody)
+        {
+            // Step 1: Validate the credentials
+            var user = ValidateUserCredentials(
+                authenticationRequestBody.UserName, authenticationRequestBody.Password);
+
+            // Step 2: Check if credentials were valid, if not, return unauthorized
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            // Step 3: Create token for validated user
+            var securityKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(_configuration["Authentication:SecretForKey"]));
+            var signingCredentials = new SigningCredentials(
+                securityKey, SecurityAlgorithms.HmacSha256);
+
+            // Step 4: Create list of claims
+            var claimsForToken = new List<Claim>();
+            claimsForToken.Add(new Claim("sub", user.UserId.ToString()));
+            claimsForToken.Add(new Claim("given_name", user.FirstName));
+            claimsForToken.Add(new Claim("family_name", user.LastName));
+            claimsForToken.Add(new Claim("city", user.City));
+
+            // Step 5: Create the token
+            var jwtSecurityToken = new JwtSecurityToken(
+                _configuration["Authentication:Issuer"],
+                _configuration["Authentication:Audience"],
+                claimsForToken,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddHours(1), 
+                signingCredentials);
+
+            // Step 6: Write the token via handler
+            var tokenToReturn = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+            // Visit jwt.io to see what the encoded token represents (not encrypted) and relies on HTTP encryption
+            return Ok(tokenToReturn);
+        }
+
+        private CityInfoUser ValidateUserCredentials(string? userName, string? password)
+        {
+            //TODO: Get from a user DB or table by checking the passed-through username/password
+            // against the DB
+
+            // Temporarily assume credentials are valid (values normally coming from DB/table)
+            return new CityInfoUser(
+                1,
+                userName ?? "",
+                "Bradley",
+                "Osborne",
+                "London");
+        }
+    }
+}
